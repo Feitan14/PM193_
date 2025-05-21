34a66048d84a8ad968cc245ff330758fe3a64f91
AndresMorales13 <andy38morales@gmail.com>
2025-05-20 20:18:00 -0600

    creación de ejercicio 2

---
+++ practica 2/ejercicio2.js
@@ -0,0 +1,7 @@
+const productos = [
+    {nombre: "laptop", precio: 12000},
+    {nombre: "celular", precio: 8000},
+    {nombre: "tableta", precio: 6000},
+    {nombre: "impresora", precio: 3000},
+];
+
