42a87864049b3a518085ebbc496e7afa175e80db
AndresMorales13 <andy38morales@gmail.com>
2025-05-20 21:30:14 -0600

    fin del segundo ejercicio

--- practica 2/ejercicio2.js
+++ practica 2/ejercicio2.js
@@ -1,7 +1,13 @@
-const productos = [
+let productos = [
     {nombre: "laptop", precio: 12000},
-    {nombre: "celular", precio: 8000},
-    {nombre: "tableta", precio: 6000},
-    {nombre: "impresora", precio: 3000},
+    {nombre: "mause", precio: 250},
+    {nombre: "teclado", precio: 750},
+    {nombre: "monitor", precio: 3000},
 ];
+// filtraremos los productos que tienen un precio mayor a 1000
 
+let productorM1000 = productos.filter(p => p.precio >=1000)
+
+//obtendremos un array con los productos que tienen un precio mayor o igual a 1000
+let nombre = productorM1000.map(p =>p.nombre);
+console.log(nombre); //["laptop", "monitor"]
