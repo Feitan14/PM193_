bf48778432991ce0b2495eefc9d56d4c1c4cb37d
AndresMorales13 <andy38morales@gmail.com>
2025-05-20 20:14:31 -0600

    ejercicio1_2

--- practica 2/persona.js
+++ practica 2/persona.js
@@ -1,3 +1,10 @@
 const persona = {
-    nombre: "Andrés Mrales"
-}
+    nombre: "Andres Morales",
+    edad: 37,
+    direccion: {
+        ciudad: "Qro",
+        pais: "MX"
+        }
+};
+
+console.log("Hola me llamo",persona.nombre,",","tengo",persona.edad, "y vivo en",persona.direccion.ciudad, persona.direccion.pais); // Qro
