7ae847619679c7508ee6d337d6fcc5d261959eed
AndresMorales13 <andy38morales@gmail.com>
2025-06-10 20:23:20 -0600

    avance clase
    actividad 4

--- blanco/App.js
+++ blanco/App.js
@@ -1,15 +1,32 @@
+/* zona1: importaciones */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
+
+const Texto= (props)=>{
+  const {contenido}=props
+  return(
+    <Text>{contenido}</Text>
+  )
+}
+
+/*zona 2 main o zona de ejecución*/
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto contenido="Hola"></Texto>
+      <Texto contenido="Mundo"></Texto>
+      <Texto contenido="ReactNative"></Texto>
+      <Button title="presionale"></Button>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+/* zona3: estilos*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
